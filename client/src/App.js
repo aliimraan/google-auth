ddc95a85392a93bdff53ef6dffc21535f927a389
aliimraan <aimraan204@gmail.com>
2021-03-28 16:18:15 +0530

    clientId changed

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
     <GoogleLogin
-    clientId="1009479810415-el4gr20tfk4dlt22ggj5agouaj6afrj2.apps.googleusercontent.com"
+    clientId="your-clientId"
     buttonText="Login"
     onSuccess={(response)=>responseGoogle(response)}
     onFailure={(response)=>responseGoogle()}
